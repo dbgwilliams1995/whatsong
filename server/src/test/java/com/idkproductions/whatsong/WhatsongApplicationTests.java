9fbe2742d65bf012fcfdfaad702ca2b00703dc4f
David Williams <david.williams@mac4086.mynet>
2020-11-06 10:50:38 +0000

    add server directory

---
+++ server/src/test/java/com/idkproductions/whatsong/WhatsongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idkproductions.whatsong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
